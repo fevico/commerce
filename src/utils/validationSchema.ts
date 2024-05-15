37f6b67f270a1673670512ae2ce37e98d93edb74
John Chinonso Edeh <netojaycee@gmail.com>
2024-05-15 22:22:08 +0100

    u

--- src/utils/validationSchema.ts
+++ src/utils/validationSchema.ts
@@ -6,7 +6,7 @@ export const productValidation = yup.object().shape({
     price:yup.number().required("Product price is required"),
     description:yup.string().required("Product description is required"),
     categoryId:yup.string().required("Product category is required"),
-    image: yup.string().required("Product image is required"),
+    image: yup.string(),
     quantity: yup.number(),
 })
 
