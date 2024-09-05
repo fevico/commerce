f580999cee1195c2d2e23f52b24312c279ee3e47
John Chinonso Edeh <netojaycee@gmail.com>
2024-09-05 02:19:39 +0100

    update

--- src/index.ts
+++ src/index.ts
@@ -14,29 +14,29 @@ import brandRouter from './routers/brand';
 const app = express();
 
 app.use(
-    cors({
-      origin: "*",
-      credentials: false,
-    })
-  );
+  cors({
+    origin: "*",
+    credentials: false,
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("src/public"));
-app.use('/category', categoryRouter);
-app.use('/product', productRouter);
-app.use('/auth', authRouter);
-app.use('/order', orderRouter);
-app.use('/payment', paymentRouter);
-app.use('/blog', blogRouter);
-app.use('/brand', brandRouter)
+app.use('/api/category', categoryRouter);
+app.use('/api/product', productRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/order', orderRouter);
+app.use('/api/payment', paymentRouter);
+app.use('/api/blog', blogRouter);
+app.use('/api/brand', brandRouter)
 
-app.use(function (err, req, res, next){
-  res.status(500).json({message: err.message})
+app.use(function (err, req, res, next) {
+  res.status(500).json({ message: err.message })
 } as express.ErrorRequestHandler)
 
 const PORT = 5004;
 
-app.listen(PORT, '0.0.0.0', ()=>{
-    console.log('Port is listening on '+ PORT);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log('Port is listening on ' + PORT);
 })
